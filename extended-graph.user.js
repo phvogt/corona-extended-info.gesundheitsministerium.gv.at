f7d93f4b9512648a56a4b9712e88c2223a851b94
Philipp Vogt <phvogt@users.noreply.github.com>
2020-03-29 13:52:30 +0200

    0.5 added interval to add diagram after reload

--- extended-graph.user.js
+++ extended-graph.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         info.gesundheitsministerium.gv.at TrendPercent extended
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Show percents
 // @author       Philipp
 // @match        https://info.gesundheitsministerium.gv.at/
@@ -9,13 +9,39 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
+/*
+Changelog:
+
+0.5 added interval to add diagram after reload
+
+ */
+
+var LOGPREFIX = "info.gesundheitsministerium.gv.at: ";
 
 (function() {
     'use strict';
 
-    window.addEventListener('load', function() {
+    var intervalId = 0;
+
+    // add the custom trend graph
+    function addCustomTrend() {
+
+        var methodname = "addCustomTrend(): ";
+//        console.log(LOGPREFIX + methodname + "start");
+
+        var customTrendDiv = document.getElementById("customTrend");
+        if (customTrendDiv) {
+//            console.log(LOGPREFIX + methodname + "customTrendDiv exists");
+            return;
+        }
 
         var dpTrend = unsafeWindow.dpTrend;
+        if (!dpTrend) {
+//            console.log(LOGPREFIX + methodname + "no dpTrend!");
+            return;
+        }
+
+        console.log(LOGPREFIX + methodname + "creating custom graph");
 
         var row = document.createElement("DIV");
         row.classList.add("row");
@@ -32,59 +58,67 @@
         var rows = document.getElementsByClassName("container-fluid")[0];
         rows.insertBefore(row, rows.firstChild);
 
-var ps=[];
-var maxi = dpTrend.length;
-for (var i=0;i<maxi-1;i++) {
-    var v1 = dpTrend[i].y;
-    var v2 = dpTrend[i+1].y;
-    var p= Math.trunc((v2/v1-1)*100);
-    console.log(p);
-    ps.push({"label" : dpTrend[i].label, "y":p, "x" : i+1});
-}
+        var ps=[];
+        var maxi = dpTrend.length;
+        for (var i=0;i<maxi-1;i++) {
+            var v1 = dpTrend[i].y;
+            var v2 = dpTrend[i+1].y;
+            var p= Math.trunc((v2/v1-1)*100);
+//            console.log(p);
+            ps.push({"label" : dpTrend[i].label, "y":p, "x" : i+1});
+        }
 
-var chart5 = new CanvasJS.Chart("customTrend", {
-                animationEnabled: true,
-                theme: "dark1",
-                title: {
-                    text: "Nationaler Trend", fontFamily: "calibri", fontSize: 20, fontWeight: "normal"
-                },
-                axisY: [{
-                    title : "Steigerung in %",
-                    titleFontColor: "#4f81bc",
-                    labelFontColor: "#4f81bc",
-                    includeZero: true,
-                    stripLines:[{
-                        value: 10
-                    }]
-                },{
-                    title : "gesamt erkrankt",
-                    titleFontColor: "#c0504e",
-                    labelFontColor: "#c0504e",
-                    includeZero: true
-                },{
-                    title : "logarithmisch gesamt erkrankt",
-                    titleFontColor: "#9bbb58",
-                    labelFontColor: "#9bbb58",
-                    logarithmic: true
-                }],
-                data: [{
-                    type: "line",
-                    axisYIndex: 0,
-                    lineColor: "#4f81bc",
-                    dataPoints: ps
-                },{
-                    type: "line",
-                    axisYIndex: 1,
-                    lineColor: "#c0504e",
-                    dataPoints: dpTrend
-                },{
-                    type: "line",
-                    axisYIndex: 2,
-                    lineColor: "#9bbb58",
-                    dataPoints: dpTrend
+        var chart5 = new CanvasJS.Chart("customTrend", {
+            animationEnabled: true,
+            theme: "dark1",
+            title: {
+                text: "Nationaler Trend", fontFamily: "calibri", fontSize: 20, fontWeight: "normal"
+            },
+            axisY: [{
+                title : "Steigerung in %",
+                titleFontColor: "#4f81bc",
+                labelFontColor: "#4f81bc",
+                includeZero: true,
+                stripLines:[{
+                    value: 10
                 }]
-            });
-chart5.render();
-    }, false);
+            },{
+                title : "gesamt erkrankt",
+                titleFontColor: "#c0504e",
+                labelFontColor: "#c0504e",
+                includeZero: true
+            },{
+                title : "logarithmisch gesamt erkrankt",
+                titleFontColor: "#9bbb58",
+                labelFontColor: "#9bbb58",
+                logarithmic: true
+            }],
+            data: [{
+                type: "line",
+                axisYIndex: 0,
+                lineColor: "#4f81bc",
+                dataPoints: ps
+            },{
+                type: "line",
+                axisYIndex: 1,
+                lineColor: "#c0504e",
+                dataPoints: dpTrend
+            },{
+                type: "line",
+                axisYIndex: 2,
+                lineColor: "#9bbb58",
+                dataPoints: dpTrend
+            }]
+        });
+        chart5.render();
+        console.log(LOGPREFIX + methodname + "done");
+    }
+
+    // MAIN method
+    var methodname = "main(): ";
+    console.log(LOGPREFIX + methodname + "start");
+
+    // call the function to add the custom trend graph
+    intervalId = setInterval(addCustomTrend, 1000);
 
 })();
